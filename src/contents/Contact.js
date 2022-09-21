30571b59fdfeed8226e30082ec966d2405639606
Midhun13 <110243906+Midhun13@users.noreply.github.com>
2022-09-21 23:18:14 +0530

    Add files via upload

---
+++ src/contents/Contact.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Social from '../components/Social';
+class Contact extends Component {
+    render() {
+        return (
+            <div className="condiv">
+            <h1 className="subtopic">Contact Me</h1>
+            <h3>Email  :   midhunbms568@gmail.com</h3>
+            <h3>Instagram   :   @d_ark_shade</h3>
+            <Social />
+            </div>
+            )
+        }
+    }
+    
+    export default Contact
+    
